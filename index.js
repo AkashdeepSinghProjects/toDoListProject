ab7bf7f6dedf4405638cd3d0dce75f5290341ba5
Akashdeep Singh <akashdeepsingh@Akashdeeps-MacBook-Pro.local>
2023-10-20 10:59:46 +1100

    clean code

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Local Database connect
+// Local Database connect and todolistDB Database created
 await mongoose.connect('mongodb://127.0.0.1/todolistDB').
 catch(error => console.error("Error: ",error.message));
 
 // User Schema and Model made through mongoose method
 const UserSchema = new mongoose.Schema({
+    _id:Number,
     userName:String ,
     list:[{name:String,items:[]}]
 })
+// colection name user is created
 const UserModel = mongoose.model("user",UserSchema);
 
 const defaultDocument = new UserModel({
+    _id: 1,
     userName:"Akash",
     list: [],
 })
-
+// home page routing
 app.get("/",async (req,res)=>{
     
     try{
@@ -33,16 +36,43 @@ app.get("/",async (req,res)=>{
             defaultDocument.save();
             result = await UserModel.findOne({userName:"Akash"}); 
         }
-        if(result){
-            res.render("index.ejs",{user:result});
-        }
-        
+        res.render("index.ejs",{user:result});
     }catch(err){
        console.error("error",err.message);
     }
-    
 });
 
+// delete route to delete topic
+app.post("/delete",(req,res)=>{
+    console.log(req.body);
+    console.log("pressed");
+    res.redirect("/");
+});
+
+// topic is added 
+app.post("/addTopic",(req,res)=>{
+    if(req.body.topic!=""){addTopic(req.body.topic);}
+    res.redirect("/");
+});
+// item will be added to corresponding topic
+app.post("/addItem",async (req,res)=>{
+    const topic = Object.keys(req.body)[0]; //  topic or heading of the list/key array
+    const enteredItem = req.body[topic];
+
+    if(enteredItem!=""){
+       const result =  await UserModel.updateOne({userName:"Akash","list.name":topic},{$push:{"list.$.items":enteredItem}});
+        res.redirect("/");
+    }
+});
+
+// app.post("/userName",(req,res)=>{
+//     if (req.body["userName"]!=""){
+//         dataObject["userName"] = req.body["userName"].charAt(0).toUpperCase() + req.body["userName"].slice(1).toLowerCase();
+//         res.redirect("/");
+//     }
+// });
+
+// topic is created by function
 async function addTopic(topic){
 
     topic = topic.charAt(0).toUpperCase() + topic.slice(1).toLowerCase();
@@ -52,53 +82,6 @@ async function addTopic(topic){
         await UserModel.updateOne({userName:"Akash"},{$push:{list:documentObject}});
     }
 }
-
-app.post("/trial",(req,res)=>{
-    console.log(req.body);
-    console.log("pressed");
-});
-
-app.post("/userName",(req,res)=>{
-    if (req.body["userName"]!=""){
-        var x=req.body["userName"]
-        dataObject["userName"] = x.charAt(0).toUpperCase() + x.slice(1).toLowerCase();
-        res.redirect("/");
-    }
-});
-
-app.post("/grocery",(req,res)=>{
-    addTopic("Grocery");
-    res.redirect("/");
-});
-
-app.post("/home",(req,res)=>{
-    addTopic("Home");
-    res.redirect("/");
-});
-
-app.post("/work",(req,res)=>{
-    addTopic("Work");
-    res.redirect("/");
-});
-
-app.post("/custom",(req,res)=>{
-    addTopic(req.body["customText"]);
-    res.redirect("/");
-});
-
-app.post("/addItem",async (req,res)=>{
-
-    const obj = JSON.parse(JSON.stringify(req.body)); 
-    const parsedBodyObject = obj;  // object with attached item/value to its topic/key
-    const topic = Object.keys(parsedBodyObject)[0]; //  topic or heading of the list/key array
-    const enteredItem = req.body[topic];
-
-    if(enteredItem!=""){
-
-       const result =  await UserModel.updateOne({userName:"Akash","list.name":topic},{$push:{"list.$.items":enteredItem}});
-        res.redirect("/");
-    }
-});
 
 app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`);
